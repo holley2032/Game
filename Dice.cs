e5db27ec57caa1877ac7c2345bd91bdefe0a365d
Kevin Austin Holley <holley2032@gmail.com>
2021-05-07 09:06:33 -0400

    Updating Dice class.

--- Dice.cs
+++ Dice.cs
@@ -9,5 +9,14 @@ namespace Game
         public int DieRoll1 { get; set; }
         public int DieRoll2 { get; set; }
         public Boolean IsDouble { get; set; }
+        public Random Die1 { get; set; }
+        public Random Die2 { get; set; }
+        //var Dice1 = new Random();
+        //var Dice2 = new Random();
+        public void RollDice() {
+            //Dice1.Next();
+
+        }
+
     }
 }
