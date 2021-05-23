b513ae51ec65b286e9fd49a1640a08635fbabb69
Kevin Austin Holley <holley2032@gmail.com>
2021-05-22 21:45:38 -0400

    Adding tile properties.

---
+++ References/TileProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.References
+{
+    class TileProperties
+    {
+        private static readonly Dictionary<string, List<string>> _ValidImprovements = new Dictionary<string, List<string>>(10)
+        {
+            {"Forest", new List<string>{"Log Cabin", "Lumber Mill", ""}
+        };
+
+        public static Dictionary<string, List<string>> ValidImprovements
+        {
+            get { return _ValidImprovements; }
+        }
+        
+    }
+}
